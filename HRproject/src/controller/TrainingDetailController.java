77eae3e940bfad54ac3c9713f0eafe324403c8ff
thomaskaviani <thomaskaviani@gmail.com>
2017-11-11 16:57:22 +0100

    Update TrainingDetailController.java

--- HRproject/src/controller/TrainingDetailController.java
+++ HRproject/src/controller/TrainingDetailController.java
@@ -16,27 +16,27 @@ public class TrainingDetailController implements Initializable{
 	protected void toAddSession(ActionEvent e) {
 			
 			Navigator.loadVista(Navigator.AddSessionView);
-		}
+	}
 		
-		/*deze view moet nog gemaakt worden
-		 * @FXML
-		protected void toLocation(ActionEvent e) {
+	/*deze view moet nog gemaakt worden
+	@FXML
+	protected void toLocation(ActionEvent e) {
 				
-				Navigator.loadVista(Navigator.LocationView);
-			}
-*/
-		@FXML
-		protected void toAddEmployee(ActionEvent e) {
+			Navigator.loadVista(Navigator.LocationView);
+	}
+	*/
+	@FXML
+	protected void toAddEmployee(ActionEvent e) {
 				
-				Navigator.loadVista(Navigator.AddEmployeeToSessionView);
-			}
+			Navigator.loadVista(Navigator.AddEmployeeToSessionView);
+	}
 		
 		
-		@Override
-		public void initialize(URL arg0, ResourceBundle arg1) {
-			// TODO Auto-generated method stub
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
 			
-		}
+	}
 	
 	
 
