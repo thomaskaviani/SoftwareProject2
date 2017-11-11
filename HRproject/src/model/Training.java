97cbd1be04e4ffecd5b7d5367f639d9b8d8ada03
Stijn <stijn.rooselaers@student.ehb.be>
2017-11-11 22:59:11 +0100

    Aanpassen van Trainingklasse nu alle klassen af zijn. bepaalde objecten gebruikten andere klasse de nog niet gemaakt waren

--- HRproject/src/model/Training.java
+++ HRproject/src/model/Training.java
@@ -8,16 +8,45 @@ public class Training {
 	private int trainingId;
 	private String name;
 	private String description;
-	//List<Book> books = new ArrayList<Book>();
+	List<Book> books = new ArrayList<Book>();
 	List<Session> sessions = new ArrayList<Session>();
-	//List <Certificate> certificate = new ArrayList<Certificate>();
-	//private Survey survey;
+	List <Certificate> certificate = new ArrayList<Certificate>();
+	private Survey survey;
 	
 	//CONSTRUCTOR LATER TOEVOEGEN
+	
 		
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	}
+
+	public Training(int trainingId, String name, String description, List<Book> books, List<Session> sessions,
+			List<Certificate> certificate, Survey survey) {
+		super();
+		this.trainingId = trainingId;
+		this.name = name;
+		this.description = description;
+		this.books = books;
+		this.sessions = sessions;
+		this.certificate = certificate;
+		this.survey = survey;
+	}
+	
+
+	public Training(int trainingId, String name, String description, List<Session> sessions) {
+		super();
+		this.trainingId = trainingId;
+		this.name = name;
+		this.description = description;
+		this.sessions = sessions;
+	}
+
+	public Training(int trainingId, String name, String description) {
+		super();
+		this.trainingId = trainingId;
+		this.name = name;
+		this.description = description;
 	}
 
 	public int getTrainingId() {
@@ -52,6 +81,31 @@ public class Training {
 		this.sessions = sessions;
 	}
 	
+	
+	public List<Book> getBooks() {
+		return books;
+	}
+
+	public void setBooks(List<Book> books) {
+		this.books = books;
+	}
+
+	public List<Certificate> getCertificate() {
+		return certificate;
+	}
+
+	public void setCertificate(List<Certificate> certificate) {
+		this.certificate = certificate;
+	}
+
+	public Survey getSurvey() {
+		return survey;
+	}
+
+	public void setSurvey(Survey survey) {
+		this.survey = survey;
+	}
+
 	public void addSession (Session session)
 	{
 		this.sessions.add(session);
@@ -67,10 +121,10 @@ public class Training {
 	    }
 		
 	}
-	/*
+	
 	 public void addBook (Book book)
 	{
-		this.books.add(Book);
+		this.books.add(book);
 	}
-*/
+
 }
