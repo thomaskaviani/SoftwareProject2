eb758db34e07ab50c4038ba805e816c5306ae867
Thomas <thomaskaviani@gmail.com>
2017-11-11 16:41:45 +0100

    TrainingDAO + Training/controller + SearchTrainingView/controller

--- HRproject/src/model/Training.java
+++ HRproject/src/model/Training.java
@@ -1,23 +1,51 @@
 package model;
-import java.util.*;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
 public class Training {
 
-	
+	@Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
 	private int trainingId;
+	
+	@Column
 	private String name;
-	private String description;
-	//List<Book> books = new ArrayList<Book>();
-	List<Session> sessions = new ArrayList<Session>();
+	@Column
+	private String goal;
+	@Column
+	private int arch;
+	
+	//List<Session> sessions = new ArrayList<Session>();
 	//List <Certificate> certificate = new ArrayList<Certificate>();
+	//List<Book> books = new ArrayList<Book>();
 	//private Survey survey;
 	
-	//CONSTRUCTOR LATER TOEVOEGEN
+	public Training() {
 		
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	}
+	
+	public Training(String name, String description) {
+		this.name = name;
+		this.goal = description;
+	}
+	
+	
+	
+	public int getArch() {
+		return arch;
+	}
 
+
+	public void setArch(int arch) {
+		this.arch = arch;
 	}
 
 	public int getTrainingId() {
@@ -36,14 +64,20 @@ public class Training {
 		this.name = name;
 	}
 
-	public String getDescription() {
-		return description;
+	public String getGoal() {
+		return goal;
 	}
 
-	public void setDescription(String description) {
-		this.description = description;
+	public void setGoal(String description) {
+		this.goal = description;
 	}
-
+	
+	@Override
+	public String toString() {
+		return "\n\nID:" + getTrainingId() + "\n" + getName() + " " + getGoal();
+	}
+	
+	/*
 	public List<Session> getSessions() {
 		return sessions;
 	}
@@ -67,7 +101,7 @@ public class Training {
 	    }
 		
 	}
-	/*
+	
 	 public void addBook (Book book)
 	{
 		this.books.add(Book);
