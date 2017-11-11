084eee7ea26ea342460d2ae40495d7457ad42b00
Thomas <thomaskaviani@gmail.com>
2017-11-11 15:55:01 +0100

    controllers Training & SearchTraining

---
+++ HRproject/src/controller/SearchTrainingController.java
@@ -0,0 +1,18 @@
+package controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class SearchTrainingController implements Initializable {
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
+
+
