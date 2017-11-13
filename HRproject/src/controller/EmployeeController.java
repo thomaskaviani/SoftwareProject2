70c8cdad1311681309d1588ad0fba59c2a0fa3ee
Thomas <thomaskaviani@gmail.com>
2017-11-13 14:47:12 +0100

    aanpassen employeecontroller

--- HRproject/src/controller/EmployeeController.java
+++ HRproject/src/controller/EmployeeController.java
@@ -3,21 +3,31 @@ package controller;
 //import application.Navigator;
 import model.Employee;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 
 
-public class EmployeeController {
+public class EmployeeController implements Initializable{
 	
 	@FXML
 	private TableView<Employee> tableView;
+	
 	@FXML 
-	private TextField firstNameField;
-    @FXML 
-    private TextField lastNameField;
-    @FXML 
-    private TextField emailField;
+	private TextField searchBar;
+    
+    
+    
+    
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		
+	}
     
     
     
