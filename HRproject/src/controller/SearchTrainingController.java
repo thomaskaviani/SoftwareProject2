3ce4a669e4108ab8110450faf8eda4ba6df30de5
Thomas <thomaskaviani@gmail.com>
2017-11-13 11:26:40 +0100

    trainingen in tabel laden

--- HRproject/src/controller/SearchTrainingController.java
+++ HRproject/src/controller/SearchTrainingController.java
@@ -3,10 +3,14 @@ package controller;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import dao.TrainingDAO;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import model.Training;
 
 public class SearchTrainingController implements Initializable {
@@ -19,9 +23,20 @@ public class SearchTrainingController implements Initializable {
 	
 	@FXML
 	private TableColumn<Training, String> trainingDescCol;
+	
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
+		
+		TrainingDAO tdao = new TrainingDAO();
+		
+		ObservableList<Training> trainingen = FXCollections.observableArrayList(tdao.getAll());
+		
+		tableView.setItems(trainingen);
+		
+		trainingNameCol.setCellValueFactory(new PropertyValueFactory<Training, String>("name"));
+		trainingDescCol.setCellValueFactory(new PropertyValueFactory<Training, String>("goal"));
+		
 		
 	}
 
