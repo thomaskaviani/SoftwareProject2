cbdfd17f960cd913bdf33f989c6fef585b76d0ed
Unknown <hasan.zamin@student.ehb.be>
2017-12-23 23:14:07 +0100

    extras
    view herwerkt en restore functie erbij geplaatst

--- HRproject/src/controller/ExtrasController.java
+++ HRproject/src/controller/ExtrasController.java
@@ -2,11 +2,13 @@
  * 
  * bronnen: https://www.youtube.com/watch?v=fq1alQY9iLk&index=5&list=LLAEkIkIY1AWlm-_N7rl1S7A, 
  * http://java-buddy.blogspot.be/2012/05/save-file-with-javafx-filechooser.html
+ * https://uwudamith.wordpress.com/2010/12/24/get-backup-and-restore-on-mysql-in-java/
  * */
 package controller;
 
 
 import java.io.File;
+
 
 
 import java.io.IOException;
@@ -40,7 +42,7 @@ public class ExtrasController implements Initializable{
 
 			try {
 				Runtime runtime = Runtime.getRuntime(); 
-				p = runtime.exec("C://Program Files//MySQL//MySQL Server 5.7//bin//mysqldump.exe -P3306 -h 172.20.0.27 -uSP2GR3_HRAPP -pCupRYb --add-drop-database SP2GR3_HRAPP -r "+ path);
+				p = runtime.exec("C://Program Files//MySQL//MySQL Server 5.7//bin//mysqldump.exe -P 3306 -h 172.20.0.27 -u SP2GR3_HRAPP -pCupRYb --add-drop-database -B SP2GR3_HRAPP -r "+ path);
 			
 			int processComplete = p.waitFor();
 			if (processComplete==0) {
@@ -53,62 +55,73 @@ public class ExtrasController implements Initializable{
 			catch (Exception e2) {
 				e2.printStackTrace();
 			}
-		}
-		
-	
-		
-		
-		
-		
+		}	
 		System.out.println("test");
-
-		
 	}
 	
-
+//voor backup
 	@FXML
 	protected void browseFile(ActionEvent e) {
 		FileChooser fc = new FileChooser();
 		
 		FileChooser.ExtensionFilter ex = new FileChooser.ExtensionFilter("SQL Files", "*.sql");
 		fc.getExtensionFilters().add(ex);
-		
-		
-		
-		
-		//String date = new SimpleDateFormat("yyyy-mm-dd").format(new Date(0));
-		
+				
 		try {
 			File f = fc.showSaveDialog(null);
 			path = f.getAbsolutePath();
-			path = path.replace('\\', '/');
 			//path = path;
 			textField.setText(path);
 			
 		} catch (Exception e2) {
 			e2.printStackTrace();
 		}
-		
-		
-		
 		System.out.println("test");
         }
-        
+     //voor restore   
+	@FXML
+	protected void browse(ActionEvent e) {
+		FileChooser fileChooser = new FileChooser();
 		
+	FileChooser.ExtensionFilter ex = new FileChooser.ExtensionFilter("SQL Files", "*.sql");
+	fileChooser.getExtensionFilters().add(ex);
+		
+	try {
+		File f = fileChooser.showOpenDialog(null);
+		path = f.getAbsolutePath();
+		path = path.replace("\\", "/");
+		textField.setText(path);
+	} catch (Exception e2) {
+		e2.printStackTrace();
+	}
+			System.out.println("test");
+	}
 	
+	
+	@FXML
+	protected void restoreDB(ActionEvent e) {
+		/*String dbUserName ="SP2GR3_HRAPP";
+		String dbPassword ="CupRYb";
+		String dbName="SP2GR3_HRAPP";*/
+	String[] restoreCmd = new String[] {"C://Program Files//MySQL//MySQL Server 5.7//bin//mysql.exe -u SP2GR3_HRAPP -pCupRYb SP2GR3_HRAPP -P 3306 -h 172.20.0.27  < " + path};
+	Process runtimeProcess;	
+	try {
+		runtimeProcess = Runtime.getRuntime().exec(restoreCmd); 
+		int processComplete = runtimeProcess.waitFor();
+	if (processComplete==0) {
+		System.out.println("Restore succes");
+	} else {
+System.out.println("Restore failed");
+	}
 		
-		
-	/*private static void configureFileChooser(final FileChooser fileChooser) {      
-        fileChooser.setTitle("Select Files");
-        
-        fileChooser.setInitialDirectory(
-            new File(System.getProperty("user.home"))
-        );          
-        
-        fileChooser.getExtensionFilters().add(
-            new FileChooser.ExtensionFilter("SQL", "(*.sql)")
-        );
-}*/
+	} catch (Exception e2) {
+		e2.printStackTrace();
+	}
+			System.out.println("test");
+	}
+	
+	
+	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		// TODO Auto-generated method stub
